a9c5f0147ecca09f5309fd3651be0df5bd1c67f9
Alan Plum <me@pluma.io>
2017-07-19 12:10:44 +0200

    Add user middleware
    Now every request has a "user" property we can use to access the authenticated user.

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ const sessions = sessionsMiddleware({
   transport: 'cookie'
 });
 module.context.use(sessions);
+
+const users = module.context.collection('users');
+module.context.use(function (req, res, next) {
+  if (req.session.uid) {
+    try {
+      req.user = users.document(req.session.uid)
+    } catch (e) {
+      req.session.uid = null;
+      req.sessionStorage.save();
+    }
+  }
+  next();
+});
