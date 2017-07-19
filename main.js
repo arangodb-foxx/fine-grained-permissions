a1d31ffabc12eba09ea709358c2850e6f6891015
Alan Plum <me@pluma.io>
2017-07-19 12:04:08 +0200

    Add session middleware
    Our request handlers now have access to a cookie-based session backed by our sessions collection.

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 module.context.use('/patients', require('./routes/patients'), 'patients');
+
+const sessionsMiddleware = require('@arangodb/foxx/sessions');
+const sessions = sessionsMiddleware({
+  storage: module.context.collection('sessions'),
+  transport: 'cookie'
+});
+module.context.use(sessions);
