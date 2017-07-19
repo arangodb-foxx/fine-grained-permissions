f659b89d34b198100026a3d7206061c4aaa949db
Alan Plum <me@pluma.io>
2017-07-19 12:07:23 +0200

    Add a login route

--- routes/auth.js
+++ routes/auth.js
@@ -7,3 +7,21 @@ const users = module.context.collection('users');
 
 const router = createRouter();
 module.exports = router;
+
+router.post('/login', function (req, res) {
+  const username = req.body.username;
+  const user = users.firstExample({username});
+  const valid = auth.verify(
+    user ? user.authData : {},
+    req.body.password
+  );
+  if (!valid) res.throw('unauthorized');
+  req.session.uid = user._key;
+  req.sessionStorage.save(req.session);
+  res.send({sucess: true});
+})
+.body(joi.object({
+  username: joi.string().required(),
+  password: joi.string().required()
+}).required(), 'Credentials')
+.description('Logs a registered user in.');
